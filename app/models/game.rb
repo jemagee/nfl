8db65103e3236e4ab5e9ac9fbf73ebf7117bf826
John Magee <jemagee@cox.net>
2017-01-02 19:18:05 -0800

    Creating the functionality for game addition from downloaded source data and testing that it works properly

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,23 @@
 class Game < ApplicationRecord
 
+	require 'Nokogiri'
+
 	has_many :participants
 
 	validates :gamedate, presence: true
 	validates :nflcomid, presence: true, uniqueness: {case_sensitive: false}, numericality: {only_integer: true}
+
+	def self.add_games(rawdata)
+		source = Nokogiri::HTML(open(rawdata))
+		games = source.css("a.gc-btn")
+		games.each do |game|
+			href = game["href"]
+			gameid = href.scan(/\/(\d+)\//)[0][0]
+			year = gameid[0..3].to_i
+			month = gameid[4..5].to_i
+			day = gameid[6..7].to_i
+			gamedate = Date.new(year,month,day)
+			Game.create(gamedate: gamedate, nflcomid: gameid)
+		end
+	end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
+
+
   it {should validate_presence_of(:gamedate)}
   it {should validate_presence_of(:nflcomid)}
   it {should validate_uniqueness_of(:nflcomid).case_insensitive}
   it {should validate_numericality_of(:nflcomid).only_integer}
   it {should have_many(:participants)}
+
+  scenario "Nokogiri will successfully populate the game and date information" do
+
+  	expect{Game.add_games('spec/fixtures/nokogiritest.html')}.to change{Game.count}.by(16)
+  end
 end
